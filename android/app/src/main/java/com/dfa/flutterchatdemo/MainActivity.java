e819f45dbfebd63ee071c815a00f0eb4b78df0b3
nightmare2207 <darkrising2207@gmail.com>
2025-11-20 17:02:23 +0700

    v 3.8.1 android set up

--- android/app/src/main/java/com/dfa/flutterchatdemo/MainActivity.java
+++ android/app/src/main/java/com/dfa/flutterchatdemo/MainActivity.java
@@ -1,69 +1,87 @@
-package hust.appchat;
+package hust.appchat
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
-import android.provider.Settings;
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugin.common.MethodChannel;
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-public class MainActivity extends FlutterActivity {
-    private static final String CHANNEL = "chat_bubble_overlay";
-    private static final int REQUEST_CODE_OVERLAY = 1000;
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "chat_bubble_overlay"
+    private val OVERLAY_PERMISSION_REQUEST = 1001
 
-    @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        super.configureFlutterEngine(flutterEngine);
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
 
-        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-                .setMethodCallHandler((call, result) -> {
-                    switch (call.method) {
-                        case "hasPermission":
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                                result.success(Settings.canDrawOverlays(this));
-                            } else {
-                                result.success(true);
-                            }
-                            break;
-
-                        case "requestPermission":
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                                if (!Settings.canDrawOverlays(this)) {
-                                    Intent intent = new Intent(
-                                            Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                                            Uri.parse("package:" + getPackageName())
-                                    );
-                                    startActivityForResult(intent, REQUEST_CODE_OVERLAY);
-                                    result.success(false);
-                                } else {
-                                    result.success(true);
-                                }
-                            } else {
-                                result.success(true);
-                            }
-                            break;
-
-                        case "showBubble":
-                            // Implement bubble display logic here
-                            result.success(true);
-                            break;
-
-                        case "hideBubble":
-                            // Implement bubble hide logic here
-                            result.success(true);
-                            break;
-
-                        case "hideAllBubbles":
-                            // Implement hide all logic here
-                            result.success(true);
-                            break;
-
-                        default:
-                            result.notImplemented();
-                            break;
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+                when (call.method) {
+            "requestPermission" -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (!Settings.canDrawOverlays(this)) {
+                        val intent = Intent(
+                                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                                Uri.parse("package:$packageName")
+                        )
+                        startActivityForResult(intent, OVERLAY_PERMISSION_REQUEST)
+                        result.success(false)
+                    } else {
+                        result.success(true)
                     }
-                });
+                } else {
+                    result.success(true)
+                }
+            }
+            "hasPermission" -> {
+                val hasPermission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    Settings.canDrawOverlays(this)
+                } else {
+                    true
+                }
+                result.success(hasPermission)
+            }
+            "showBubble" -> {
+                val userId = call.argument<String>("userId")
+                val userName = call.argument<String>("userName")
+                val avatarUrl = call.argument<String>("avatarUrl")
+
+                if (userId != null) {
+                    val intent = Intent(this, ChatBubbleService::class.java).apply {
+                        action = ChatBubbleService.ACTION_SHOW_BUBBLE
+                        putExtra("userId", userId)
+                        putExtra("userName", userName)
+                        putExtra("avatarUrl", avatarUrl)
+                    }
+                    startService(intent)
+                    result.success(true)
+                } else {
+                    result.success(false)
+                }
+            }
+            "hideBubble" -> {
+                val userId = call.argument<String>("userId")
+                if (userId != null) {
+                    val intent = Intent(this, ChatBubbleService::class.java).apply {
+                        action = ChatBubbleService.ACTION_HIDE_BUBBLE
+                        putExtra("userId", userId)
+                    }
+                    startService(intent)
+                    result.success(true)
+                } else {
+                    result.success(false)
+                }
+            }
+            "hideAllBubbles" -> {
+                val intent = Intent(this, ChatBubbleService::class.java).apply {
+                    action = ChatBubbleService.ACTION_HIDE_ALL
+                }
+                startService(intent)
+                result.success(true)
+            }
+                else -> result.notImplemented()
+        }
+        }
     }
 }
